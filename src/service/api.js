12cf2674bd2c81d22b4660058a87eec8730551f7
WYseven <consolelog@126.com>
2019-08-25 22:07:53 +0800

    添加接口说明

--- src/service/api.js
+++ src/service/api.js
@@ -1,88 +1,183 @@
 import axios from 'axios';
-import { async } from 'q';
 
-axios.interceptors.request.use(
-  config => {
-    let token = localStorage.getItem('token');
-    if (token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
-      config.headers.authorization = `token ${token}`;
+class HttpRequest {
+  constructor(options){
+    this.defaults = {
+      baseUrl: ''
     }
-    return config;
-  },
-  err => {
-    return Promise.reject(err);
+    this.defaults = Object.assign(this.defaults, options);
   }
-);
+  setConfig(){
+
+  }
+
+  interceptors(install){
+    install.interceptors.request.use(
+      config => {
+        let token = localStorage.getItem('token');
+        if (token) {  // 判断是否存在token，如果存在的话，则每个http header都加上token
+          config.headers.authorization = `token ${token}`;
+        }
+        return config;
+      },
+      err => {
+        return Promise.reject(err);
+      }
+    );
+    install.interceptors.response.use(
+      res => {
+        const { data, status } = res;
+        return data;
+      },
+      err => {
+        return Promise.reject(err);
+      }
+    );
+  }
+  request(options){
+    options = Object.assign(this.defaults, options)
+    console.log(options);
+    const instance = axios.create(options)
+    this.interceptors(instance);
+    return instance
+  }
+}
+
+const request = new HttpRequest({
+  baseURL: '/api'
+});
+
+const http = request.request();
+
 
 // 获取所有属性分类
 export async function getProperty(){
-  let data = await axios.get('/api/menu/property');
-  return data.data;
+  return await http.get('/menu/property');
 }
 // 获取所有菜谱分类
 export async function getClassify(){
-  let data = await axios.get('/api/menu/classify');
-  return data.data;
+  return await http.get('/menu/classify');
 }
 
-// 发布菜谱
+/**
+ * 发布菜谱
+ * @export
+ * @param {Object} params - 参考mock的数据
+ * @returns
+ */
 export async function publish(params){
-  console.log(JSON.stringify(params, null, 2))
-  let data = await axios.post('/api/menu/publish', params);
-  return data.data;
+  return await http.post('/menu/publish', params);
 }
 
-// 注册账号
+/**
+ * 注册账号
+ * @export
+ * @param {Object} params - 
+ * @param {Object} params.name - 用户名
+ * @param {Object} params.password - 密码
+ * @returns
+ */
 export async function register(params){
-  let data = await axios.post('/api/user/create', params);
-  return data.data;
+  return await http.post('/user/create', params);
 }
-// 登录
+
+/**
+ * 账号登录
+ * @export
+ * @param {Object} params - 
+ * @param {string} params.name - 用户名
+ * @param {string} params.password - 密码
+ * @returns
+ */
 export async function login(params){
-  let data = await axios.post('/api/user/login', params);
-  return data.data;
+  return await http.post('/user/login', params);
 }
-// 用户信息
+
+/**
+ * 获取用户信息
+ * @export
+ * @param {Object} params - 
+ * @param {string} params.userId - 用户id
+ * @returns
+ */
 export async function userInfo(params){
-  let data = await axios.post('/api/user/info', params);
-  return data.data;
+  return await http.post('/user/info', params);
 }
-// 拿到用户发布的菜谱
+
+/**
+ * 拿到用户发布的菜谱，可做筛使用选
+ * @export
+ * @param {Object} [params] - 不填写，则获取所有的菜谱
+ * @param {string} [params.userId] - 指定用户的菜单
+ * @param {string} [params.classify] - 按照菜单分类，进行筛选
+ * @param {string} [params.property] - 指定菜单属性进行筛选
+ * @returns
+ */
 export async function getMenus(params){
-  let data = await axios.get('/api/menu/query', {params});
-  return data.data;
+  return await http.get('/menu/query', {params});
 }
-// 根据id，拿到菜谱的详细信息
+
+/**
+ * 根据菜单id，拿到菜谱的详细信息
+ * @export
+ * @param {Object} [params] - 
+ * @param {string} [params.menuId] - 指定菜单的id
+ * @returns
+ */
 export async function menuInfo(params){
-  let data = await axios.get('/api/menu/menuInfo', {params});
-  return data.data;
+  return await http.get('/menu/menuInfo', {params});
 }
 
-// toggle 收藏{menuId}
-
+/**
+ * toggle 收藏。收藏的取消收藏；没收藏的，要收藏。
+ * @export
+ * @param {Object} [params] - 
+ * @param {string} [params.menuId] - 指定要收藏的菜单的id
+ * @returns
+ */
 export async function toggleCollection(params){
-  let data = await axios.post('/api/user/collection', params);
-  return data.data;
+  return await http.post('/user/collection', params);
 }
-// 收藏
-export async function collection(params){
-  let data = await axios.get('/api/user/collection', {params});
-  return data.data;
+/**
+ * toggle 关注。关注的取消关注；没关注的，要关注。
+ * @export
+ * @param {Object} [params] - 
+ * @param {string} [params.followUserId] - 指定要关注的用户的id
+ * @returns
+ */
+export async function toggleFollowing(params){
+  return await http.post('/user/following', params);
 }
 
-// toggle关注 {followUserId}
-export async function toggleFollowing(params){
-  let data = await axios.post('/api/user/following', params);
-  return data.data;
+/**
+ * 获取指定用户的收藏的菜单
+ * @export
+ * @param {Object} [params] - 
+ * @param {string} [params.userId] - 指定的用户id
+ * @returns
+ */
+export async function collection(params){
+  return await http.get('/user/collection', {params});
 }
-// 我的关注
+
+/**
+ * 获取指定用户关注的人
+ * @export
+ * @param {Object} [params] - 
+ * @param {string} [params.userId] - 指定的用户id
+ * @returns
+ */
 export async function following(params){
-  let data = await axios.get('/api/user/following', {params});
-  return data.data;
+  return await http.get('/user/following', {params});
 }
-// 我的粉丝
+
+/**
+ * 获取指定用户的粉丝
+ * @export
+ * @param {Object} [params] - 
+ * @param {string} [params.userId] - 指定的用户id
+ * @returns
+ */
 export async function fans(params){
-  let data = await axios.get('/api/user/fans', {params});
-  return data.data;
+  return await http.get('/user/fans', {params});
 }
-// 我的收藏
